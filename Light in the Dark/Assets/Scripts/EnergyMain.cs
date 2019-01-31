7576c5098149b65d2dbb33b8b710a49f600269f4
Tony <toscarreks@gmail.com>
2019-01-31 12:44:49 -0800

    Added a Scripts folder, started working on an Energy class

---
+++ Light in the Dark/Assets/Scripts/EnergyMain.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyMain
+{
+
+    // Main Energy "Data Struct" I guess you could call this
+    // The UI pieces should all directly access this
+    // Things like Room or EnergyHandler or something
+
+    const int MAX_ENERGY = 100;
+    private int energy = 100;
+
+    int take(){
+        if(energy > 0){
+            energy--;
+        }
+        return 1;
+    }
+
+    void add(){
+        if(energy < MAX_ENERGY){
+            energy++;
+        }
+    }
+
+    int get(){
+        return energy;
+    }
+}
