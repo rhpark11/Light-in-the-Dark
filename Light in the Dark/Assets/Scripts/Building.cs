3914ad996721a74df5d26461f22b32aec33efd0e
Baddie123 <baddie1350@gmail.com>
2019-02-01 12:31:41 -0800

    Added a GameManager and a test building
    energy can now go back and forth from main bar to building.

---
+++ Light in the Dark/Assets/Scripts/Building.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    
+    public float Energy = 0f; // how much energy this building currently has
+    
+    public float maxEnergy;    // how much energy can this building take
+
+    public GameManager gameManager;
+    
+    public float changeEnergy; // how much energy the building takes 
+
+    
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>(); // should only be one GameManager! more like a (EnergyManager)
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Energy < maxEnergy)
+            {
+                gameManager.TakeEnergy(10f); // public function from the GameManager class.
+                Energy += changeEnergy;
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            releaseEnergy();
+        }
+    }
+
+    public void releaseEnergy()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Energy -= changeEnergy;
+            GameManager.Energy += changeEnergy;
+        }
+    }
+    
+}
---
+++ Light in the Dark/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static float Energy = 100f;
+    public Image energyBar;
+
+    private float maxEnergy;
+    public float dEnergy = 10f;
+    
+    void Start()
+    {
+        maxEnergy = Energy;
+    }
+
+   
+    void Update()
+    {
+        if (Energy >= maxEnergy)
+        {
+            Energy = maxEnergy;
+        }
+        
+        energyBar.fillAmount = Energy / maxEnergy;
+    }
+
+    public void TakeEnergy(float dEnergy)
+    {
+        Energy -= dEnergy; // lost dEnergy amound of energy called from other object
+    }
+}
