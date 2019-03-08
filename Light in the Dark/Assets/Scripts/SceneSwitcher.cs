4bc966dc1d0ae5eda89ddadb6b99bca3f73283e1
Tony <toscarreks@gmail.com>
2019-03-08 14:42:45 -0800

    Added a mainmenu

---
+++ Light in the Dark/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitcher : MonoBehaviour
+{
+
+    public string scene_name;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    public void loadScene(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene_name);
+    }
+}
